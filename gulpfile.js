4115227ebc8284e34b46b7e94f1a820260afbb90
mburger89 <mburger89@me.com>
2017-04-12 00:09:12 -0700

    gulp task: auto build and reload working

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,35 @@
 var gulp = require('gulp');
 var serve = require('gulp-serve');
 var nunjucksRender = require('gulp-nunjucks-render');
+var browserSync = require('browser-sync').create();
 
 gulp.task('serve', serve('app'));
 
 gulp.task('build', function() {
-  //gets .html and .nunjucks files in pages
-  return gulp.src('app/pages/**/*.+(html|nunjucks)')
-  //Renders template with nunjucks
-  .pipe(nunjucksRender({
-    path: ['app/templates']
-  }))
-  //outputs files in app folder
-  .pipe(gulp.dest('app'))
+    //gets .html and .nunjucks files in pages
+    return gulp.src('app/pages/**/*.+(html|nunjucks)')
+        //Renders template with nunjucks
+        .pipe(nunjucksRender({
+            path: ['app/templates']
+        }))
+        //outputs files in app folder
+        .pipe(gulp.dest('app'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
-gulp.task('buildServe', ['build'], serve('app')); 
 
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app/'
+        },
+    })
+})
+
+gulp.task('watch', ['browserSync', 'build'], function() {
+    gulp.watch('app/templating/partials/**/*.+(html|nunjucks)', ['build']);
+    gulp.watch('app/templating/**/*.+(html|nunjucks)', ['build']);
+    gulp.watch('app/pages/**/*.+(html|nunjucks)', ['build']);
+});
